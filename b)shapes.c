06a6460cfe5d7f5f52b4a90221fb13cf041505fe
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-13 02:18:18 +0530

    Update and rename b)basics.c to b)shapes.c

---
+++ b)shapes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main(){
+    
+    /*
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n");
+    */
+    
+    printf("  *  \n *** \n*****");
+    
+    // Rectangles / Asterisks Patterns
+    printf("1*********\n");
+    printf("12********\n");
+    printf("123*******\n");
+    printf("1234******\n");
+    printf("12345*****\n\n\n");
+
+
+    // printf("**********\n");
+    // printf("**********\n");
+    // printf("**********\n");
+    // printf("**********\n");
+    // printf("**********\n");
+    printf("**********\n**********\n**********\n**********\n**********\n");
+
+    // X Shape
+//     *     *
+//      *   *  
+//       * *
+//        *
+//       * *
+//      *   *
+//     *     *
+    printf("*     *\n");
+    printf(" *   *\n");
+    printf("  * *\n");
+    printf("   *\n");
+    printf("  * *\n");
+    printf(" *   *\n");
+    printf("*     *\n");
+}
