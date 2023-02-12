fc26c21efbe60c9085a6c87b795ce5cfc8e9dcec
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-12 23:18:47 +0530

    Add files via upload

---
+++ a)printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World! \nAanya \nthis \nside\n");
+    printf("First Name: Aanya \nLast Name: Panigrahi\n");
+    // printf("First Name: Aanya");
+    // printf("\nLast Name: Panigrahi");
+    // \n -> new line character
+    return 0;
+}
