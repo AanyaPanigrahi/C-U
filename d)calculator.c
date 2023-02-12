b512fda7668d7ef4bf952d089c4c243bebb27cf5
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-12 23:19:33 +0530

    Add files via upload

---
+++ d)calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("Sum %d + %d = %d",5,2,5+2);
+    printf("\nDifference %d - %d = %d",5,2,5-2);
+    printf("\nMultiple %d * %d = %d",5,2,5*2);
+    printf("\nDivision %d / %d = %d",5,2,5/2);
+    printf("\nModulus %d %% %d = %d",5,2,5%2);
+    // for modulus i.e. to print remainder as output use double mod %%
+}
