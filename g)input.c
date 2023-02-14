c48e8c3a1b47fb8ebc8d5349f9079bdf6c4d23bd
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-14 22:51:06 +0530

    Add files via upload

---
+++ g)input.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    // int grade1;
+    // int grade2;
+    // grade1=80;
+    // grade2=100;
+    // printf("Average = %d\n",(grade1+grade2)/2);
+
+    int num;
+    scanf("%d",&num);
+    printf("num is %d\n\n",num);
+    
+    int num1;
+    int num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    printf("Average = %d\n",(num1+num2)/2);
+
+}
