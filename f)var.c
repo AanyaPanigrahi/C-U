7a2e0afa86695c4f850b89808433586cb6749e20
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-14 22:50:36 +0530

    Add files via upload

---
+++ f)var.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // Var type, name, content, address
+    // Var creation/declaration, assignment, reading input from user
+    // type of variable, variable name ;
+    // int for integers, double for float point values
+    int age; // var decl.
+    age=30; // var assign.
+    double temp;
+    temp=24.2;
+
+}
