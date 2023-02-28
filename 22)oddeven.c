b9c49cca33118f9b280fd94c6745bda0d508d1cd
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-02-28 23:47:26 +0530

    Add files via upload

---
+++ 22)oddeven.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    printf("enter the number : ");
+    scanf("%d",&num);
+    if (num%2==0)
+        printf("%d is an even number",num);
+    else
+        printf("%d is an odd number",num);
+    return 0;
+}
